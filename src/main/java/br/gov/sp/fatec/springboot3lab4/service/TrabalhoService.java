5a89e83dd64358f1e5feac6a34c0193ee790062a
Kauã Borgarelli <borgarellitavares@outlook.com>
2024-04-15 22:22:16 +0000

    new changes

--- src/main/java/br/gov/sp/fatec/springboot3lab4/service/TrabalhoService.java
+++ src/main/java/br/gov/sp/fatec/springboot3lab4/service/TrabalhoService.java
@@ -1,8 +1,13 @@
 package br.gov.sp.fatec.springboot3lab4.service;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
+import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
+
+import br.gov.sp.fatec.springboot3lab4.entity.Trabalho;
 import br.gov.sp.fatec.springboot3lab4.repository.TrabalhoRepository;
 
 @Service
@@ -10,6 +15,16 @@ public class TrabalhoService {
     
     @Autowired
     private TrabalhoRepository repository;
+
+    public Trabalho newTrabalho(Trabalho trabalho) {
+        if(trabalho == null || trabalho.getTra_titulo().isBlank())
+        throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Dados de usuário inválidos!");
+        return repository.save(trabalho);
+    }
+
+    public List<Trabalho> getAllTrabalhos() {
+        return repository.findAll();
+    }
 
     
 }
